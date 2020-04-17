7f5f2fb2a85e18fb034791dbeb83b85f7137001f
Carl Javier B. Engo <57130927+cjengo033@users.noreply.github.com>
2020-04-18 06:45:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,26 +3,23 @@ window.onload = function() {
 }
 
 function getCovidStats() {
-	fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations/182')
-	.then(function(resp) { return resp.json() })
+	fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations/182')// api of the Philippines
+	.then(function(resp) { 
+		return resp.json() }
+	     )
 	.then(function(data) {
 		let population = data.location.country_population;
 		let update = data.location.last_updated;
 		let confirmedCases = data.location.latest.confirmed;
 		let deaths = data.location.latest.deaths;
-		let recovered = data.location.latest.deaths;
 
 		document.getElementById('population').innerHTML = population.toLocaleString('en');
 		document.getElementById('update').innerHTML = update.substr(0, 10);
 		document.getElementById('cases').innerHTML = confirmedCases.toLocaleString('en');
 		document.getElementById('deaths').innerHTML = deaths.toLocaleString('en');
-
-	
-
-
 	})
 	.catch(function() {
 		console.log("error");
 	})
-	setTimeout(getCovidStats, 23200000) // update every 12 hours
+	setTimeout(getCovidStats, 23200000) 
 }
